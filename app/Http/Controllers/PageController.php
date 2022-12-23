e7d8cfd9baa46f5779eeb39ad0c1b1037df706a2
ferdyhape <ferdyhahan5@gmail.com>
2022-12-23 18:47:41 +0700

    add comment variable

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Feed;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -12,7 +13,8 @@ class PageController extends Controller
     {
         return view('relationship_tes', [
             "title" => "Relationship Test",
-            "feed" => Feed::all()
+            "feed" => Feed::all(),
+            "comment" => Comment::all(),
         ]);
     }
 }
