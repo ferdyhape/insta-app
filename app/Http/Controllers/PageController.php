c5022795527e91c79d0df3fc15dbd1e95f3a79c9
ferdyhape <ferdyhahan5@gmail.com>
2022-12-23 14:51:47 +0700

    add PageController

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Feed;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function relationship_test()
+    {
+        return view('relationship_tes', [
+            "title" => "Relationship Test",
+            "feed" => Feed::all()
+        ]);
+    }
+}
