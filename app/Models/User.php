97d3c0061bb6240bb532774beb96888545b94e88
ferdyhape <ferdyhahan5@gmail.com>
2022-12-23 12:58:04 +0700

    add phone column in user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'profile_photo_path',
+        'phone',
         'password',
     ];
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('profile_photo_path')->unique();
+            $table->string('phone')->unique();
+            $table->string('profile_photo_path')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
