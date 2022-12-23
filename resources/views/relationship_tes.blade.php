00f72da79eaa01ca674628671f7de9686ff8197b
ferdyhape <ferdyhahan5@gmail.com>
2022-12-23 18:46:02 +0700

    modify relationship_tes view for testing all table

--- resources/views/relationship_tes.blade.php
+++ resources/views/relationship_tes.blade.php
@@ -1,4 +1,19 @@
 @foreach ($feed as $f)
-{{$f->caption }}<br>
-{{$f->user->name }}<br>
+<h5>Nama: {{$f->user->name }}</h5>
+<h5>Caption: {{$f->caption }}, {{$f->like }} Likes</h5>
+
+{{-- menampilkan yang like --}}
+@if (($f->like) > 0)
+<p>disukai oleh:
+    @foreach ($f->like_details as $lk)
+    {{ $lk->user->name }},
+    @endforeach
+</p>
+@endif
+
+{{-- menampilkan komen --}}
+@foreach ($f->comments as $c)
+<h5>@ {{$c->user->name}}: {{$c->content}}</h5>
+@endforeach
+<p>---------------------------------</p>
 @endforeach
