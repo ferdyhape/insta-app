1bac378eab04e672793b7b1bf0c6bb11b9515a39
ferdyhape <ferdyhahan5@gmail.com>
2022-12-24 14:13:59 +0700

    modify route add name for FeedController

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FeedController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('index');
-})->name('home')->middleware('auth');
 
 Route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
 Route::get('/register', [AuthController::class, 'register'])->name('register')->middleware('guest');
@@ -26,3 +13,12 @@ Route::post('/login', [AuthController::class, 'authenticate'])->name('authentica
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout_process');
 
 Route::get('/relat', [PageController::class, 'relationship_test'])->name('relationship');
+Route::get('/like/{id}', [LikeController::class, 'like'])->name('like_process');
+
+// Route::resource('/feed', FeedController::class)->middleware('auth');
+
+Route::resource('/feed', FeedController::class, [
+    'names' => [
+        'index' => 'home',
+    ]
+])->middleware('auth');
